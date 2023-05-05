ec862b0f14f2ec372b4ec4c9233fe429c0fd3213
KevynJH <124405397+KevynJH@users.noreply.github.com>
2023-05-04 21:53:47 -0300

    Add files via upload

---
+++ LMR/JavaScriptOperadores/script.js
@@ -0,0 +1,3 @@
+let NOME;
+NOME = prompt('Entre com seu nome:', 'Digite-o aqui');
+document.write('fala fera ' + NOME + ' fique a vontade.');
---
+++ LMR/JavaScriptOperadores/script1.js
@@ -0,0 +1,10 @@
+let numero1;
+let numero2;
+let resposta;
+
+numero1 = prompt('Digite um valor:', 'Digite um número');
+numero2 = prompt('Digite outro valor:', 'Digite um número');
+//realiza a soma de dois nº inteiros
+resposta = parseInt(numero1) + parseInt(numero2);
+
+document.write('O resultado da soma é ', resposta);
