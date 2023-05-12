d8932f9cdd8de9644992d4fc543ab43bf6a3eb22
KevynJH <124405397+KevynJH@users.noreply.github.com>
2023-05-11 21:52:12 -0300

    Add files via upload

---
+++ JavaScriptFormulario/script2.js
@@ -0,0 +1,7 @@
+function mostrarFuncao(){
+pnumero1 = document.getElementById('MyCalc').pfuncao1.value;
+pnumero2 = document.getElementById('MyCalc').pfuncao2.value;
+pnumero3 = document.getElementById('MyCalc').pfuncao3.value;
+pnumero4 = document.getElementById('MyCalc').pfuncao4.value;
+alert('seu resultado é '+resultado);
+}
