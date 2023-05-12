3b91ca7184e0d50e32bc7777be6f1f1fff31012e
KevynJH <124405397+KevynJH@users.noreply.github.com>
2023-05-11 21:54:11 -0300

    Delete JavaScriptFormulario directory

--- JavaScriptFormulario/script1.js
+++
@@ -1,8 +0,0 @@
-function MostrarNome() {
-
-    let Nome = document.getElementById('MeuForm').pnome.value;
-    let Sobrenome = document.getElementById('MeuForm').psobre.value;
-    let NomeCompleto = Nome + ' ' + Sobrenome;
-    alert('Olá ' + NomeCompleto);
-
-}
