39e63742ef211beae4a0ae31e2d7e18267555f7b
KevynJH <124405397+KevynJH@users.noreply.github.com>
2023-05-11 21:49:37 -0300

    Add files via upload

---
+++ LMR/javaScriptIFelse/script2.js
@@ -0,0 +1,11 @@
+let diaSemana;
+
+diaSemana = prompt("Informe um nº","1-7");
+
+if(diaSemana==1){document.write("Domingo");}
+if(diaSemana==2){document.write("Segunda-feira");}
+if(diaSemana==3){document.write("Terça-feira");}
+if(diaSemana==1){document.write("Quarta-feira");}
+if(diaSemana==1){document.write("Quinta-feira");}
+if(diaSemana==1){document.write("Sexta-feira");}
+if(diaSemana==1){document.write("Sábado");}
---
+++ LMR/javaScriptIFelse/script3.js
@@ -0,0 +1,19 @@
+let diaSemana;
+
+diaSemana = prompt("Informe um nº", "1-7");
+
+if (diaSemana == 1) {
+    document.write("Domingo");
+} else if (diaSemana == 2) {
+    document.write("Segunda-feira")
+} else if (diaSemana == 3) {
+    document.write("Terça-feira")
+} else if (diaSemana == 1) {
+    document.write("Quarta-feira")
+} else if (diaSemana == 1) {
+    document.write("Quinta-feira")
+} else if (diaSemana == 1) {
+    document.write("Sexta-feira")
+} else if (diaSemana == 1) {
+    document.write("Sábado")
+}
