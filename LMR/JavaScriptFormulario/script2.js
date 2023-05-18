e9eef60eeae03b44a855d44307743a37c603cb53
KevynJH <124405397+KevynJH@users.noreply.github.com>
2023-05-18 20:06:55 -0300

    Delete LMR/JavaScriptFormulario directory

--- LMR/JavaScriptFormulario/script2.js
+++
@@ -1,7 +0,0 @@
-function mostrarFuncao(){
-pnumero1 = document.getElementById('MyCalc').pfuncao1.value;
-pnumero2 = document.getElementById('MyCalc').pfuncao2.value;
-pnumero3 = document.getElementById('MyCalc').pfuncao3.value;
-pnumero4 = document.getElementById('MyCalc').pfuncao4.value;
-alert('seu resultado é '+resultado);
-}
